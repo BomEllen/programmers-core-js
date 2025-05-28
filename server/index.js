3c27ef28a5e268221e6b33bc36297d8f6fa5229f
BomEllen <jjjj200100@gmail.com>
2025-05-28 15:20:23 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// console.log(navigator.language);
+
+
+const server = require('live-server');
+
+const params = {
+  host: 'localhost',
+  port: 3000,
+  open: false,
+  root:'./client'
+}
+
+server.start(params)
+
+
+
